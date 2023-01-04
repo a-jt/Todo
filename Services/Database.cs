cc8d16f8f5a2508af6b49ea4a6bd008b18d426b5
Aaron <Aaron@IEVI>
2023-01-04 12:19:53 +0000

    Add project files.

---
+++ Services/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Models;
+
+namespace Todo.Services
+{
+    public class Database 
+    {
+        public IEnumerable<TodoItem> GetItems() => new[] 
+        {
+            new TodoItem { Description = "Walk the dog"},
+            new TodoItem { Description = "Buy some milk"},
+            new TodoItem { Description = "Learn C", IsChecked = true },
+        };
+    }
+
+}
---
+++ ViewModels/AddItemViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using ReactiveUI;
+using Todo.Models;
+
+namespace Todo.ViewModels
+{
+    class AddItemViewModel : ViewModelBase
+    {
+        string description;
+
+        public AddItemViewModel() 
+        {
+            var okEnabled = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            Ok = ReactiveCommand.Create(
+                () => new TodoItem { Description = Description },
+                okEnabled);
+            Cancel = ReactiveCommand.Create(() => { });
+        }
+
+        public string Description 
+        {
+            get => description; 
+            set => this.RaiseAndSetIfChanged(ref description, value); 
+        }
+
+        public ReactiveCommand<Unit, TodoItem> Ok { get;}
+        public ReactiveCommand<Unit, Unit> Cancel { get; }
+    }
+
+}
